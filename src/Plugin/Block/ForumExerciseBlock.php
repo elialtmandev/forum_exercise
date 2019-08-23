0c66a50b71a6a12ed590a37593445d2473ee9911
spelcheck <eli@workit.us>
2019-08-22 22:19:30 -0700

    cleanup, translations and profile_link more legit url fromRoute method

--- src/Plugin/Block/ForumExerciseBlock.php
+++ src/Plugin/Block/ForumExerciseBlock.php
@@ -6,7 +6,6 @@ use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Session\AccountInterface;
-//use Drupal\Core\Session\AccountProxy;
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Link;
 use Drupal\Core\Url;
@@ -18,6 +17,9 @@ use Drupal\Core\Url;
  *   id = "forum_exercise",
  *   admin_label = @Translation("Forum Exercise"),
  *   category = @Translation("Forum Exercise"),
+ *   context = {
+ *     "user" = @ContextDefinition("entity:user", label = @Translation("User"))
+ *   }
  * )
  */
 class ForumExerciseBlock extends BlockBase implements ContainerFactoryPluginInterface {
@@ -45,7 +47,6 @@ class ForumExerciseBlock extends BlockBase implements ContainerFactoryPluginInte
   /**
    * {@inheritdoc}
    */
-  // prevent block from being initialized for anonymous users (resulting in error)
   public function blockAccess(AccountInterface $account) {
     return AccessResult::allowedIf($account->isAuthenticated());
   }
@@ -54,29 +55,15 @@ class ForumExerciseBlock extends BlockBase implements ContainerFactoryPluginInte
    * {@inheritdoc}
    */
   public function build() {
-//    $user = \Drupal\user\Entity\User::load($this->current_user->id());
     $user = $this->current_user->getAccount();
     $user_name = $this->current_user->getDisplayName();
-dsm(drupal_get_user_timezone(), "user tz");
-//    $user_last_login_prep = new DateTime($user->login, $user_timezone);
-    $user_last_login = date('F jS, Y g:ia', $user->login); // : date('F jS, Y g:ia', \Drupal::time()->getCurrentTime()); //$user->login;
-dsm($user_last_login);
+    $user_last_login = date('F jS, Y g:i a', $user->login); 
 
-    $markup[] = "Hello " . $user_name . "!";
-    $markup[] = "Your last login was " . $user_last_login;
-    $markup[] = "<a href='/user'>Visit your profile</a>";
-
-    $markup = implode($markup, "<br>");
-dsm($markup, "markup");
-
-dsm(Link::fromTextAndUrl("Visit your profile", Url::fromUserInput('/user')), "buildlink");
-    
-    $content['hello_message'] = "Hello " . $user_name . "!";
+    $content['hello_message'] = $this->t("Hello ") . $user_name . "!";
     $content['last_login'] = $this->t("Your last login was a") . $user_last_login . '.';
-    $content['profile_link'] = Link::fromTextAndUrl("Visit your profile", Url::fromUserInput('/user'));
+    $content['profile_link'] = Link::fromTextAndUrl($this->t("Visit your profile"), Url::fromRoute('user.page'));
 
     return [
-//      '#markup' => $markup,
       '#theme' => 'forum_exercise_block',
       '#content' => $content,
       '#cache' => array(
@@ -84,7 +71,6 @@ dsm(Link::fromTextAndUrl("Visit your profile", Url::fromUserInput('/user')), "bu
           'session'
         )
       ),
-//      '#cache' => array('max-age' => 0),
     ];
   }
 }
